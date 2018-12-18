2062256e3caad32e0618432f72f5a6f89a246be5
Seth Hoenig <hoenig@indeed.com>
2018-12-18 08:06:59 -0700

    noissue : a little more comprehensive help text

--- internal/cli/usage.go
+++ internal/cli/usage.go
@@ -3,9 +3,40 @@ package cli
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
+const usageText = `
+ABOUT
+
+	taggit - create and manage Go module version tags
+
+USAGE
+
+	list
+		show valid version tags in the repository
+
+	zero
+		create an initial v0.0.0 tag
+
+	patch [pre-release]
+		create an incremented patch version tag (vA.B.C+1)
+		if pre-release is specified, it is appended as an extension (vA.B.C-rc1)
+
+	minor [pre-release]
+		create an incremented minor version tag (vA.B+1.C)
+		if pre-release is specified, it is appended as an extension (vA.B.C-rc1)
+
+	major [pre-release]
+		create an incremented major version tag (v(A+1.B.C)
+		if pre-release is specified, it is appended as an extension (vA.B.C-rc1)
+
+	updates [apply]
+		list newer versions available for outdated modules in use
+		if apply is specified, automatically update the go.mod file
+`
+
 func Usage(code int) {
-	fmt.Println("usage: taggit [list, zero, patch, minor, major]")
+	fmt.Println(strings.TrimSpace(usageText))
 	os.Exit(code)
 }
