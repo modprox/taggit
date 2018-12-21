e962b589c9651b0202ef4f9355ce8ad63413f837
Seth Hoenig <hoenig@indeed.com>
2018-12-20 17:36:10 -0700

    wip sadfasdf

---
+++ internal/updates/updates.go
@@ -0,0 +1,34 @@
+package updates
+
+// make taggit do this
+// but with prettier output
+
+/*
+
+$ go list -m -u all
+go: finding golang.org/x/sys latest
+go: finding github.com/jinzhu/copier latest
+github.com/modprox/taggit
+github.com/boltdb/bolt v1.3.1
+github.com/cactus/go-statsd-client v3.1.1+incompatible
+github.com/davecgh/go-spew v1.1.1
+github.com/go-sql-driver/mysql v1.4.0 [v1.4.1]
+github.com/gorilla/context v1.1.1
+github.com/gorilla/csrf v1.5.1
+github.com/gorilla/mux v1.6.2
+github.com/gorilla/securecookie v1.1.1
+github.com/jinzhu/copier v0.0.0-20180308034124-7e38e58719c3
+github.com/lib/pq v1.0.0
+github.com/modprox/mp v0.0.5 [v0.0.10]
+github.com/pkg/errors v0.8.0
+github.com/pmezard/go-difflib v1.0.0
+github.com/shoenig/atomicfs v0.1.1
+github.com/shoenig/petrify/v4 v4.0.2
+github.com/shoenig/regexplus v0.0.0
+github.com/shoenig/toolkit v1.0.0
+github.com/stretchr/objx v0.1.1
+github.com/stretchr/testify v1.2.2
+golang.org/x/sys v0.0.0-20180909124046-d0be0721c37e [v0.0.0-20181217223516-dcdaa6325bcb]
+google.golang.org/appengine v1.1.0 [v1.3.0]
+
+*/
